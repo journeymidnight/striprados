419ce0241fa699e05d5a0f2a6165877a1f2c75e0
root <root@bj-yz3-ceph-14-10.(none)>
2016-11-08 15:48:11 +0800

    bug fix Signed-off-by: root <root@bj-yz3-ceph-14-10.(none)>

--- striprados.c
+++ striprados.c
@@ -174,16 +174,16 @@ int is_cached(const char * entry) {
 int do_ls(rados_ioctx_t ioctx) {
 	int ret;
 	const char *entry;
-	rados_list_ctx_t *list_ctx;
+	rados_list_ctx_t list_ctx;
 	char buf[128];
 	int length;
-	ret = rados_objects_list_open(ioctx, list_ctx);
+	ret = rados_objects_list_open(ioctx, &list_ctx);
 	if (ret < 0) {
 		debug("error reading list");
 		return -1;
 	}
 	debug("===striper objects list===\n");
-	while(!quit && rados_objects_list_next(*list_ctx, &entry, NULL) != -ENOENT) {
+	while(!quit && rados_objects_list_next(list_ctx, &entry, NULL) != -ENOENT) {
 		if (is_cached(entry) == 0)
 			continue;
 		if ((length = is_head_object(entry)) == 0)
@@ -703,23 +703,21 @@ void process_remove_ver_objs(void *arg){
 int do_clear_old_files(rados_striper_t striper, rados_ioctx_t ioctx, const char *key, int force) {
 	int ret;
 	const char *entry;
-	rados_list_ctx_t *list_ctx;
-	char buf[128];
+	rados_list_ctx_t list_ctx;
+	char buf[1024];
 	int length;
 	uint64_t size;
 	time_t mod_time;
 	time_t now_time;
 	int date_of_expiry = atoi(key)*24*60*60;
 	rm_args_t args = NULL;
-	threadpool tp;
-	tp = create_threadpool(50);
-	ret = rados_objects_list_open(ioctx, list_ctx);
+	ret = rados_objects_list_open(ioctx, &list_ctx);
 	if (ret < 0) {
 			debug("error reading list");
 			return -1;
 	}
 	debug("===start delete objects ===\n");
-	while(!quit && rados_objects_list_next(*list_ctx, &entry, NULL) != -ENOENT) {
+	while(!quit && rados_objects_list_next(list_ctx, &entry, NULL) != -ENOENT) {
 		if (is_cached(entry) == 0)
 			continue;
 		if ((length = is_head_object(entry)) == 0)
@@ -735,15 +733,7 @@ int do_clear_old_files(rados_striper_t striper, rados_ioctx_t ioctx, const char 
 		}
 		time(&now_time);
 		if ((now_time - mod_time) > date_of_expiry){
-			if (multi){
-				add_obj_to_list(&rlist, buf);
-				args = (rm_args_t)malloc(sizeof(rm_args));
-				args->io_ctx = ioctx;
-				args->striper = striper;
-				dispatch_threadpool(tp, process_remove_ver_objs, (void *)args);
-			}else{
-				striprados_remove(ioctx, striper, buf);
-			}
+			striprados_remove(ioctx, striper, buf);
 		}
 	}
 	
@@ -751,7 +741,6 @@ int do_clear_old_files(rados_striper_t striper, rados_ioctx_t ioctx, const char 
 	while(!list_empty(&rlist.job_list)){
 		sleep(5);
 	}
-	destroy_threadpool(tp);
 	debug("===all objects deleted complete ===\n");
 	return 0;
 }
